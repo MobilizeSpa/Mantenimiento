5899d6b376969d5b4dfaa84adbd5c8b3285f59c5
olanoit <olanoit@gmail.com>
2021-11-07 14:30:01 -0500

    propuesta de dependencias de tareas

--- l10n_cl_maintenance/models/guideline_activity.py
+++ l10n_cl_maintenance/models/guideline_activity.py
@@ -1,10 +1,14 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import Warning
+from odoo.exceptions import Warning, ValidationError
 
 
 class MaintenanceGuidelineActivity(models.Model):
     _name = 'guideline.activity'
     _description = 'Guideline Activity'
+    _parent_name = "parent_id"
+    _parent_store = True
+    _rec_name = 'complete_name'
+    _order = 'complete_name'
     _check_company_auto = True
 
     sequence = fields.Integer(required=True, default=10)
@@ -12,6 +16,33 @@ class MaintenanceGuidelineActivity(models.Model):
 
     code = fields.Char(string='Code', required=True, copy=False)
     name = fields.Char(string='Name', required=True, copy=False)
+
+    complete_name = fields.Char(
+        'Complete Name', compute='_compute_complete_name',
+        store=True)
+    parent_id = fields.Many2one('guideline.activity', 'Parent Activity',
+                                index=True, ondelete='cascade', check_company=True)
+    parent_path = fields.Char(index=True)
+    child_ids = fields.One2many('guideline.activity', 'parent_id', 'Child Actities')
+
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for category in self:
+            if category.parent_id:
+                category.complete_name = '%s / %s' % (category.parent_id.complete_name, category.name)
+            else:
+                category.complete_name = category.name
+
+    @api.constrains('parent_id')
+    def _check_category_recursion(self):
+        if not self._check_recursion():
+            raise ValidationError(_('You cannot create recursive activities.'))
+        return True
+
+    @api.model
+    def name_create(self, name):
+        return self.create({'name': name}).name_get()[0]
+
     att_documents = fields.Many2many('ir.attachment', string='Documents', required=False)
     description = fields.Html(string='Description', required=False)
 
